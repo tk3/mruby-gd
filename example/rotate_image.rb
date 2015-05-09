cd93fc7be97164b29e187f505a2c66dbd85f2554
qtakamitsu <qtakamitsu@gmail.com>
2015-05-09 21:28:27 +0900

    Modify rename load image APIs The following API are renamed: - new_from_gif -> new_from_gif_file - new_from_png -> new_from_png_file - new_from_jpeg -> new_from_jpeg_file - new_from_wbmp -> new_from_wbmp_file - new_from_tiff -> new_from_tiff_file - new_from_bmp -> new_from_bmp_file

--- example/rotate_image.rb
+++ example/rotate_image.rb
@@ -1,4 +1,4 @@
-im = GD::Image.new_from_jpeg 'in.jpeg'
+im = GD::Image.new_from_jpeg_file 'in.jpeg'
 im_out = GD::Image.new im.width, im.height
 im.copy_rotated im_out, im.width/2, im.height/2, 0, 0, im.width, im.height, 30
 im_out.jpeg_file 'rotate.jpeg', 90
